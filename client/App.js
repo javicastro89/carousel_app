6d472bd5bd5d7b397e8dc99e61aeecc905ecc849
Javi <javiercastroloureiro@gmail.com>
2021-08-23 13:09:52 -0300

    las commit

--- client/App.js
+++ client/App.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import {styles} from './styles'
-import { Text, View, Image, Button, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { styles } from './styles'
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
@@ -8,56 +8,51 @@ export default function App() {
 
   const [blocks, setBlocks] = useState([])
   const [page, setPage] = useState(0)
-  
-  
 
-  const getImages= () => {
+
+
+  const getImages = () => {
 
     axios('https://mini-carousel-app.herokuapp.com/images')
-    .then(result =>{ 
-      setBlocks(()=>result.data)
+      .then(result => {
+        setBlocks(() => result.data)
       })
-    .catch(error => console.error(error))
+      .catch(error => console.error(error))
   }
-  
+
 
   const handleBttn = (event) => {
 
-    if(event === 'next') setPage((prev) => prev += 1)
+    if (event === 'next') setPage((prev) => prev += 1)
     else setPage((prev) => prev -= 1)
 
   }
 
   const storeData = async (value) => {
+
     try {
-      
       await AsyncStorage.setItem('page', value.toString())
-      
+
     } catch (error) {
-
       console.error(error)
-
     }
   }
 
   const getData = async () => {
+
     try {
-
       const result = await AsyncStorage.getItem('page')
-      if(result) setPage(parseInt(result))
-      
-      
-    } catch (error) {
+      if (result) setPage(parseInt(result))
 
+    } catch (error) {
       console.error(error)
 
     }
   }
 
   useEffect(() => {
-    
-    if(page !== 0) storeData(page)
 
+    if (page !== 0) storeData(page)
   }, [page])
 
   useEffect(() => {
@@ -66,51 +61,47 @@ export default function App() {
     getData()
 
   }, [])
-  
+
   return (
     <View style={styles.container}>
 
       {blocks.length > 0 ?
-      <View>
-      <View style={styles.titleContainer}>
-      <Text style={styles.textStyle}>{blocks[page].title}</Text>
-      </View>
-      <View style={styles.imageContainer}>
-      {blocks[page].images.map((element) => 
-          
-          <Image 
-          key={element}
-          style={styles.imageStyle} 
-          source={{uri: element}}
-          />  
-          
-          )}
-      </View>
+        <View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.textStyle}>{blocks[page].title}</Text>
+          </View>
+          <View style={styles.imageContainer}>
+            {blocks[page].images.map((element) =>
 
-      </View>
-          : <Text style={styles.textStyle}>Loading...</Text>}
+              <Image
+                key={element}
+                style={styles.imageStyle}
+                source={{ uri: element }}
+              />
 
-      
+            )}
+          </View>
+
+        </View>
+        : <Text style={styles.textStyle}>Loading...</Text>}
 
       <View style={styles.buttons}>
 
-     
- 
-      <TouchableOpacity
-        onPress={() => handleBttn('prev')}
-        style={page === 0 ? styles.bttnDisable : styles.bttStyle}
-        disabled={page === 0}
-      >
-        <Text style={page === 0 ? styles.textBttnDisable : styles.textBttn}>{'<'}</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => handleBttn('prev')}
+          style={page === 0 ? styles.bttnDisable : styles.bttStyle}
+          disabled={page === 0}
+        >
+          <Text style={page === 0 ? styles.textBttnDisable : styles.textBttn}>{'<'}</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => handleBttn('next')}
-        style={blocks.length > 0 && page === blocks.length - 1 ? styles.bttnDisable : styles.bttStyle}  
-        disabled={blocks.length > 0 ? page === blocks.length - 1 : false}
-      >
-        <Text style={blocks.length > 0 && page === blocks.length - 1 ? styles.textBttnDisable : styles.textBttn}>{'>'}</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => handleBttn('next')}
+          style={blocks.length > 0 && page === blocks.length - 1 ? styles.bttnDisable : styles.bttStyle}
+          disabled={blocks.length > 0 ? page === blocks.length - 1 : false}
+        >
+          <Text style={blocks.length > 0 && page === blocks.length - 1 ? styles.textBttnDisable : styles.textBttn}>{'>'}</Text>
+        </TouchableOpacity>
 
       </View>
 
